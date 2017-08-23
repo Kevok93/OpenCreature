30aa9f16fc572a20deca647d7b652bb201e46cf0
kevok <kevok93@gmail.com>
2017-08-23 14:22:29 -0400

    Still WIP, partial creation of moves.cs

--- Assets/Scripts/Objects/Moves.cs
+++ Assets/Scripts/Objects/Moves.cs
@@ -1,18 +1,33 @@
-using System;
+using System.Collections;
+using System.Collections.Generic;
 
-public class Moves
+public class Move
 {
+	public Dictionary<int,Move> MOVES;
 	int id;
 	string name;
 	byte pp, power, accuracy;
 	Type movetype;
+	int world_effect_id, battle_effect_id;
 	Effect world_effect, battle_effect;
 	MoveAffinity affinity;
 	bool[] misc_info;
 	byte misc_value;
 	string description;
 
-	public Moves(
+	private Move(){}
+	public static void init(List<Dictionary<string,string>> move_defs) {
+		MOVES = new Dictionary<int, Type>(move_defs.Count);
+		foreach (Dictionary<string,string> row in move_defs) {
+			Move temp = MOVES[id] = new Move ();
+			temp.id = System.Convert.ToInt32 (row ["id"]);
+			temp.name = row["name"];
+			temp.pp = System.Convert.ToByte (row ["pp"]);
+			temp.power = System.Convert.ToByte (row ["power"]);
+			temp.
+		}
+	}
+	public Move(
 		int id,
 		string name,
 		byte pp, 
