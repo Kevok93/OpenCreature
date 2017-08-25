b28f94b38a3a5926257057fb106b10993f16182e
kevok <kevok93@gmail.com>
2017-08-25 16:33:29 -0400

    Unity autodisable did not work as expected, fixed.

--- Assets/Scripts/UnityLogAppender.cs
+++ Assets/Scripts/UnityLogAppender.cs
@@ -6,21 +6,24 @@ using log4net.Core;
 
 public class UnityLogAppender : AppenderSkeleton {
 	protected override void Append(LoggingEvent e) {
-		#if UNITY_EDITOR
+		#if !COMMAND_LINE
 		var level = e.Level;
 		switch(level.ToString()) {
-			case "Trace":
-			case "Debug":
-			case "Info":
-			case "Notice":
+			case "TRACE":
+			case "DEBUG":
+			case "INFO":
+			case "NOTICE":
 				UnityEngine.Debug.Log(e.RenderedMessage);
 				break;
-			case "Warn":
+			case "WARN":
 				UnityEngine.Debug.LogWarning(e.RenderedMessage);
 				break;
-			case "Error":
-			case "Critical":
+			case "ERROR":
+			case "CRITICAL":
 				UnityEngine.Debug.LogError(e.RenderedMessage);
+				break;
+			default:
+				log4net.Util.LogLog.Error(typeof(UnityLogAppender),"Log level "+level.ToString()+" Not found!");
 				break;
 		}
 		#endif
